36d05755cc058a58f9b75c98cfd5c986c5413206
khoirul mutaqin <100250229+KhoirulMutaqin08@users.noreply.github.com>
2026-02-23 10:45:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -349,7 +349,7 @@ function generateKmlFromMatches(matches) {
       <name>TE</name>
       <description><![CDATA[
         ${nama}
-        T;E;7
+T;E;7
       ]]></description>
       <Point>
         <coordinates>${lng},${lat}</coordinates>
